a30eadbecd5f0e895f2ccbc47e0c0923560aeee3
ME-nna <135108479+ME-nna@users.noreply.github.com>
2025-10-17 23:35:17 +0300

    test2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-//test for successful action
+//test for successful action2
 public class Main {
     public static void main(String[] args) {
         int x = 10;
