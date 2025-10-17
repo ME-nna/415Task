2e3270229314d275a6049473a3582b20ef996c15
ME-nna <135108479+ME-nna@users.noreply.github.com>
2025-10-17 23:00:08 +0300

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int x = 10;
+        int y = 5;
+        int result = (x * y) + x + y + 2;
+        System.out.println("--- Starting Calculation ---");
+        System.out.println("Value of x: " + x);
+        System.out.println("Value of y: " + y);
+        System.out.println("Result of (x * y) + x + y + 2 is: " + result);
+        System.out.println("--- Calculation Complete ---");
+    }
+}
