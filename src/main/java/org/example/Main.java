d18cf80a9b715440638df4a2a03c234a2df27b29
ME-nna <135108479+ME-nna@users.noreply.github.com>
2025-10-17 23:16:31 +0300

    Test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        //test that everything is okaaay
         int x = 10;
         int y = 5;
         int result = (x * y) + x + y + 2;
